c0808e12edb70b890aa584a3dac39307b19443c8
BlueT - Matthew - 練喆明 <BlueT@BlueT.org>
2017-02-12 21:53:43 +0800

    bump version to 0.04

--- lib/AnyEvent/XMLRPC.pm
+++ lib/AnyEvent/XMLRPC.pm
@@ -21,11 +21,11 @@ AnyEvent::XMLRPC - Non-Blocking XMLRPC Server. Originally a AnyEvent implementat
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
